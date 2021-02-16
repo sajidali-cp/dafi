1e41ef3d53ce27ac51d7ac772c16fbd83866736e
sajid ali <sajidalicodingpixel@gmail.com>
2021-02-16 11:11:24 +0500

    token allocation removed

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -11,7 +11,7 @@ const Home = () => {
         <>
             <div className={styles.wrapper}>
                 <HomeHeader />
-                <TokenAllocation />
+                {/* <TokenAllocation /> */}
                 <HomeDemand />
                 <HomeFeatures />
                 <Footer/>
