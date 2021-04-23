7a097e2c1a279ea475289ea756280aceda0749cc
Hur Abbas <hurabbascp@gmail.com>
2021-04-23 14:25:44 +0500

    loader + homepage buttons

--- src/components/_common/HomeHeader/HomeHeader.tsx
+++ src/components/_common/HomeHeader/HomeHeader.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import styles from "./HomeHeader.module.scss"
+import styles from "./HomeHeader.module.scss";
 import MainNavigation from "../MainNavigation/MainNavigation";
-import headerIllustration from "../../../assets/images/home-header-illustration.png"
+import headerIllustration from "../../../assets/images/home-header-illustration.png";
 import { Link } from "react-router-dom";
 
 const HomeHeader = () => {
@@ -10,19 +10,19 @@ const HomeHeader = () => {
       <MainNavigation />
       <div className={styles.header}>
         <div className={styles.headerInfo}>
-          <div className={styles.headerHeading}>
-            A Synthetic DeFi Protocol
-          </div>
+          <div className={styles.headerHeading}>A Synthetic DeFi Protocol</div>
           <div className={styles.headerText}>
-            Creating a new inflation model for every Blockchain and
-            DeFi application. DAFI, not DeFi.
+            Creating a new inflation model for every Blockchain and DeFi
+            application. DAFI, not DeFi.
           </div>
           <Link to="/app">
-            <button className={styles.headerButton}>Go to Dashboard</button>
+            <button className={`${styles.headerButton} ${styles.btnEffect}`}>
+              Go to Dashboard
+            </button>
           </Link>
         </div>
         <div className={styles.headerIllustration}>
-          <img src={headerIllustration} alt="Header Illustration"/>
+          <img src={headerIllustration} alt="Header Illustration" />
         </div>
       </div>
     </div>
